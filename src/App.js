4f959f7f9b40f56dac5f0220004b0a740666462a
isaquequadros <isaquequadros1@gmail.com>
2024-01-23 17:10:06 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,277 @@
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Rodape from "./componentes/Rodape";
+import Time from "./componentes/Time";
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+
+  const [times, setTimes] = useState([
+    {
+      id: uuidv4(),
+      nome: 'Programação',
+      cor: '#57C278'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Front-End',
+      cor: '#82CFFA'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Data Science',
+      cor: '#A6D157'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Devops',
+      cor: '#E06B69'
+    },
+    {
+      id: uuidv4(),
+      nome: 'UX e Design',
+      cor: '#DB6EBF'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Mobile',
+      cor: '#FFBA05'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Inovação e Gestão',
+      cor: '#FF8A29'
+    },
+  ]);
+
+  const inicial = [
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Ana G.',
+      cargo: 'Desenvolvedora de software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Roger A.',
+      cargo: 'Engenheiro de Software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Victor s.',
+      cargo: 'Desenvolvedor',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Cesar B.',
+      cargo: 'CEO',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[0].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Ana G.',
+      cargo: 'Desenvolvedora de software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Roger A.',
+      cargo: 'Engenheiro de Software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Victor s.',
+      cargo: 'Desenvolvedor',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Cesar B.',
+      cargo: 'CEO',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[1].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Ana G.',
+      cargo: 'Desenvolvedora de software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Roger A.',
+      cargo: 'Engenheiro de Software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Victor s.',
+      cargo: 'Desenvolvedor',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Cesar B.',
+      cargo: 'CEO',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[2].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Ana G.',
+      cargo: 'Desenvolvedora de software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Roger A.',
+      cargo: 'Engenheiro de Software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Victor s.',
+      cargo: 'Desenvolvedor',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Cesar B.',
+      cargo: 'CEO',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[3].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Ana G.',
+      cargo: 'Desenvolvedora de software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Roger A.',
+      cargo: 'Engenheiro de Software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Victor s.',
+      cargo: 'Desenvolvedor',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Cesar B.',
+      cargo: 'CEO',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[4].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Ana G.',
+      cargo: 'Desenvolvedora de software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[5].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Roger A.',
+      cargo: 'Engenheiro de Software',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[5].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Victor s.',
+      cargo: 'Desenvolvedor',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[5].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Cesar B.',
+      cargo: 'CEO',
+      imagem: 'https://media.istockphoto.com/id/1300845620/pt/vetorial/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=1024x1024&w=is&k=20&c=LkYph5lGSQQjIjx0YyV3yoPKw7w5Q5kuW_yCfTRYQZs=',
+      time: times[5].nome
+    },
+  ]
+
+  const [colaboradores, setColaboradores] = useState(inicial)
+
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
+  }
+
+  function mudarCor(cor, id) {
+    setTimes(times.map(time => {
+      if (time.id === id) {
+        time.cor = cor;
+      }
+      return time;
+    }));
+  }
+
+  function cadastrarTime({ nome, cor }) {
+    setTimes([...times, { nome, cor, id: uuidv4() }])
+  }
+
+  function resolverFavorito(id) {
+    setColaboradores(colaboradores.map(colaborador => {
+      if (colaborador.id === id) colaborador.favorito = !colaborador.favorito;
+      return colaborador;
+    }))
+  }
+
+
+  return (
+    <div>
+      <Banner />
+      <Formulario aoCriarTime={cadastrarTime} times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <section className="times">
+        <h1>Minha organização</h1>
+        {times.map((time, indice) => <Time mudarCor={mudarCor} key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} aoDeletar={deletarColaborador} aoFavoritar={resolverFavorito} />)}
+      </section>
+      <Rodape />
+    </div>
+  );
+}
+
+export default App;
